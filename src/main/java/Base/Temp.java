7b47836772e51bc8a6f468e5747957dcb5ac739e
Sakthi <Healthland>
2020-01-23 23:25:50 +0530

    file chenge to git

--- src/main/java/Base/Temp.java
+++ src/main/java/Base/Temp.java
@@ -1,5 +1,32 @@
 package Base;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 public class Temp {
 
-}
+	
+	
+	
+	
+	@Test
+	public void mini() {
+		System.out.println("gid practisefor ours");
+	}
+	
+	
+	
+	@BeforeSuite 
+	public void msg() {
+		System.out.println("before suite first execution ");
+	}
+	@AfterSuite	
+	public void  huge () {
+		System.out.println("after suite execute last one execution");
+	}
+		
+		
+		
+	}
+
